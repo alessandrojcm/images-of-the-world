d907cd9bc7feeb96c75e5f20d257d74dbaf72079
Alessandro Cuppari <me@alessandrojcm.dev>
2020-07-11 19:37:47 -0400

    chore: configured tailwind

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'tailwindcss/dist/base.min.css';
+import tw from 'twin.macro';
+
+const GreenDiv = tw.div`text-blue-500 text-3xl`;
 
 const App = () => {
-    return <div>Hello!</div>;
+    return <GreenDiv>Hello!</GreenDiv>;
 };
 
 ReactDOM.render(<App />, document.getElementById('root'));
