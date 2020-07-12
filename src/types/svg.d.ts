dca6b3f4b533d06f94eac316775514e6fbbbc8c3
Alessandro Cuppari <me@alessandrojcm.dev>
2020-07-12 18:03:41 -0400

    feat: color palette

---
+++ src/types/svg.d.ts
@@ -0,0 +1,5 @@
+declare module '*.svg' {
+    import { ComponentType } from 'react';
+    const content: ComponentType;
+    export default content;
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,22 @@
+const red = '#AA1D4E';
+const yellow = '#CDC569';
+const orange200 = '#DCD3B6';
+
 module.exports = {
     purge: ['./src/**/*.html', './src/**/*.{j,t}sx'],
     theme: {
+        colors: {
+            primary: red,
+            secondary: yellow,
+            neutral: orange200,
+            red,
+            yellow,
+            orange: {
+                '100': '#EEEEE3',
+                '200': orange200,
+            },
+            black: '#223E3F',
+        },
         extend: {},
     },
     variants: {},
