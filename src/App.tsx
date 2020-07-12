a2dd8d3ce0964ba5cec58c39fb8fa6ce411b1b72
Alessandro Cuppari <me@alessandrojcm.dev>
2020-07-12 18:04:21 -0400

    feat: layout component

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import MainLayout from './components/Layout';
+
+const App: React.FC = () => {
+    return <MainLayout>Hello!</MainLayout>;
+};
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,6 @@ import React from 'react';
 import { render } from 'react-dom';
 
 import 'tailwindcss/dist/base.min.css';
-import App from './components/App';
+import App from './App';
 
 render(<App />, document.getElementById('root'));
