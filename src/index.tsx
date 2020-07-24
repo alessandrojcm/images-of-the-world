65a6062dabf42d17442324c09a79955d8d2a27c8
Alessandro Cuppari <me@alessandrojcm.dev>
2020-07-24 19:59:42 -0400

    feat: react query devtools

--- src/index.tsx
+++ src/index.tsx
@@ -2,18 +2,27 @@ import React from 'react';
 import { render } from 'react-dom';
 
 import App from './App';
-import 'panic-overlay';
 
 import './core/i18n';
 import 'tailwindcss/dist/base.min.css';
 
 async function renderApp() {
-  if (process.env.NODE_ENV === "development") {
-    // eslint-disable-next-line no-console
-    console.warn('Running in dev mode, activating error overlay for React.');
-    await import('panic-overlay');
-  }
-  render(<App />, document.getElementById('root'));
+    if (process.env.NODE_ENV === 'development') {
+        // eslint-disable-next-line no-console
+        console.warn('Running in dev mode, activating error overlay for React.');
+        await import('panic-overlay');
+        const { ReactQueryDevtools } = await import('react-query-devtools');
+
+        render(
+            <>
+                <ReactQueryDevtools />
+                <App />
+            </>,
+            document.getElementById('root')
+        );
+    } else {
+        render(<App />, document.getElementById('root'));
+    }
 }
 
 renderApp();
