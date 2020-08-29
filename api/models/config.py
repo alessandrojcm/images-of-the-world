7a4cfa22c6015acb90e759cff9743c989b5ae294
Alessandro Cuppari <me@alessandrojcm.dev>
2020-08-29 16:15:58 -0400

    feat: faunadb configuration

--- api/models/config.py
+++ api/models/config.py
@@ -1,6 +1,7 @@
-from pydantic import BaseConfig, SecretStr
+from pydantic import BaseConfig, SecretStr, AnyHttpUrl
 
 
 class Config(BaseConfig):
     FAUNADB_SECRET_KEY: SecretStr
     DEBUG: bool = False
+    FAUNA_DB_URL: AnyHttpUrl = "http://localhost:8443"
