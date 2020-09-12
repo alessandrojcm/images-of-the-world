fce213ab51d2d43c1ce512e2b0f6ff4a134965eb
Alessandro Cuppari <me@alessandrojcm.dev>
2020-09-11 22:56:16 -0400

    feat: method to create journey

--- src/core/apis/iotwApi.tsx
+++ src/core/apis/iotwApi.tsx
@@ -2,13 +2,23 @@ import ky from 'ky-universal';
 import { from } from 'rxjs';
 
 import { pluck } from 'rxjs/operators';
-import { IImageSeller } from '~types/models';
+import { IJourneyCreation } from '~types/models';
+import { ICurrentUserContext } from '~types/props';
 
 const api = ky.create({
     prefixUrl: process.env.IOTW_API,
 });
 
-const getSellers = from(api.get('sellers').json() as Promise<{ sellers: IImageSeller[] }>).pipe(pluck('sellers'));
+const createJourney = (user: Omit<ICurrentUserContext, 'userLoggedIn' | 'loading' | 'journeyId'>) =>
+    from(
+        api
+            .post('journey', {
+                json: user,
+            })
+            .json() as Promise<IJourneyCreation>
+    );
+
+const getSellers = (journeyId: string) => from(api.get(`journey/${journeyId}`).json() as Promise<IJourneyCreation>).pipe(pluck('sellers'));
 
 // eslint-disable-next-line
-export { getSellers };
+export { getSellers, createJourney };
